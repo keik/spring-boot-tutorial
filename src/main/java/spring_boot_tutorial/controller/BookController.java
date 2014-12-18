4529c97b439d3bec39558f277e14e57a43227107
keik <k4t0.kei@gmail.com>
2014-12-18 18:18:45 +0900

    Fix to retrieve data via service

--- src/main/java/spring_boot_tutorial/controller/BookController.java
+++ src/main/java/spring_boot_tutorial/controller/BookController.java
@@ -1,20 +1,26 @@
 package spring_boot_tutorial.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import spring_boot_tutorial.domain.book.Book;
+import spring_boot_tutorial.service.BookService;
+
 @Controller
 @RequestMapping("/books")
 public class BookController {
 
+    @Autowired
+    BookService bookService;
+
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
-        List<String> books = Arrays.asList("foo", "bar", "foobar");
+        List<Book> books = bookService.findAllBook();
         model.addAttribute("books", books);
         return "book-index";
     }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (id, title, author, publisher) VALUES (1, 'Spring3入門', '長谷川 裕一', '技術評論社');
+INSERT INTO book (id, title, author, publisher) VALUES (2, '計算機プログラムの構造と解釈', 'ジェラルド・ジェイ・サスマン', 'ピアソン');
+INSERT INTO book (id, title, author, publisher) VALUES (3, '軽快なJava', 'ブルース・A. テイト', 'オライリージャパン');
